7d6a53db75eecd7b4c4612e3b9cb2e4c7751a149
Blackrush <blackrushx@gmail.com>
2013-11-13 14:40:33 +0100

    some useful new parameters

--- src/org/photon/common/persist/Parameters.scala
+++ src/org/photon/common/persist/Parameters.scala
@@ -1,6 +1,8 @@
 package org.photon.common.persist
 
-import java.sql.{ResultSet, PreparedStatement}
+import java.sql.{Timestamp, ResultSet, PreparedStatement}
+import com.twitter.util.{Duration, Time}
+import scala.concurrent.duration.{TimeUnit, MILLISECONDS}
 
 object Parameters {
   implicit object ByteParameter extends Parameter[Byte] {
@@ -36,6 +38,48 @@ object Parameters {
     def get(rs: ResultSet, name: String) = rs.getString(name)
 
     def set(ps: PreparedStatement, index: Int, value: String) = ps.setString(index, value)
+  }
+
+  implicit object TimeParameter extends Parameter[Time] {
+    def get(rs: ResultSet, index: Int) = Time(rs.getTimestamp(index))
+    def get(rs: ResultSet, name: String) = Time(rs.getTimestamp(name))
+
+    def set(ps: PreparedStatement, index: Int, value: Time) = ps.setTimestamp(index, new Timestamp(value.inMilliseconds))
+  }
+
+  implicit object FloatParameter extends Parameter[Float] {
+    def get(rs: ResultSet, index: Int) = rs.getFloat(index)
+    def get(rs: ResultSet, name: String) = rs.getFloat(name)
+
+    def set(ps: PreparedStatement, index: Int, value: Float) = ps.setFloat(index, value)
+  }
+
+  implicit object DoubleParameter extends Parameter[Double] {
+    def get(rs: ResultSet, index: Int) = rs.getDouble(index)
+    def get(rs: ResultSet, name: String) = rs.getDouble(name)
+
+    def set(ps: PreparedStatement, index: Int, value: Double) = ps.setDouble(index, value)
+  }
+
+  implicit object BooleanParameter extends Parameter[Boolean] {
+    def get(rs: ResultSet, index: Int) = rs.getBoolean(index)
+    def get(rs: ResultSet, name: String) = rs.getBoolean(name)
+
+    def set(ps: PreparedStatement, index: Int, value: Boolean) = ps.setBoolean(index, value)
+  }
+
+  implicit def OptionParameter[T](implicit p: Parameter[T], ev: Null <:< T) = new Parameter[Option[T]] {
+    def get(rs: ResultSet, index: Int) = Option(p.get(rs, index))
+    def get(rs: ResultSet, name: String) = Option(p.get(rs, name))
+
+    def set(ps: PreparedStatement, index: Int, value: Option[T]) = p.set(ps, index, value.orNull)
+  }
+
+  implicit def DurationParameter(implicit timeUnit: TimeUnit = MILLISECONDS) = new Parameter[Duration] {
+    def get(rs: ResultSet, index: Int) = Duration(rs.getLong(index), timeUnit)
+    def get(rs: ResultSet, name: String) = Duration(rs.getLong(name), timeUnit)
+
+    def set(ps: PreparedStatement, index: Int, value: Duration) = ps.setLong(index, value.inUnit(timeUnit))
   }
 
 }
